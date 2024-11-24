d34107ce930da7733e290c2f3ba524b89a902871
Abhishek005vk <abhishek005vk@gmail.com>
2024-11-24 22:03:09 +0530

    first commit

--- dbconnection.js
+++ dbconnection.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 async function dbconnection() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/vaartalaap", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      process.env.MONGO_URL || "mongodb://localhost:27017/vaartalaap",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("Database Connected");
   } catch (error) {
     console.log("Error while Connceting with the database : ", error);
